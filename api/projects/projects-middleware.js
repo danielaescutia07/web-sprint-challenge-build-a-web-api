b2b5784adcc29c5b5624ee08a4339d7865ac9f13
danielaescutia07 <danielaescutia@yahoo.com>
2021-12-03 14:55:07 -0600

    get and get by id are working

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,44 @@
 // add middlewares here related to projects
+const Project = require('./projects-model');
+
+async function validateProjectId(req, res, next) {
+    try{
+        const project = await Project.get(req.params.id)
+        if (project) {
+            req.project = project
+            next()
+        } else {
+            res.status(404).json({
+                message: 'not found'
+            })
+        }
+    } catch (error) {
+        next(error)
+    }
+}
+
+function validateProject(req, res, next) {
+    if (!req.body.name) {
+        next({ status: 400 })
+    } else {
+        next()
+    }
+}
+
+function validateActions(req, res, next) {
+
+}
+
+function errorHandling(err, req, res, next) {
+    res.status(err.status || 500).json({
+        message: `Project router: ${err.message}`,
+        stack: err.stack
+    })
+}
+
+module.exports = {
+    validateProjectId,
+    validateProject,
+    validateActions,
+    errorHandling
+}
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,26 +1,57 @@
 // Write your "projects" router here!
 const express = require('express');
 
+const {
+    validateProjectId,
+    validateProject,
+    validateActions,
+    errorHandling
+} = require('./projects-middleware');
+
+const Project = require('./projects-model');
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.json('it is working!')
+router.get('/', (req, res, next) => {
+    Project.get()
+        .then(projects => {
+            if (projects) {
+                res.status(200).json(projects)
+            } else {
+                res.json([])
+            }
+        })
+        .catch(next)
 })
-router.get('/:id', (req, res) => {
+
+router.get('/:id', validateProjectId, (req, res) => {
+    res.json(req.project)
 
 })
-router.post('/', (req, res) => {
-
+router.post('/', validateProject, (req, res, next) => {
+    Project.insert(req.body)
+        .then(createdProject => {
+            res.status(201).json(createdProject)
+        })
+        .catch(next)
 })
-router.put('/:id', (req, res) => {
-
+router.put('/:id', validateProjectId, (req, res, next) => {
+    Project.update()
+        .then()
+        .catch(next)
 })
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id', validateProjectId, (req, res, next) => {
+    Project.remove()
+        .then()
+        .catch()
 })
-router.get('/:id/actions', (req, res) => {
-
+router.get('/:id/actions', validateProjectId, (req, res, next) => {
+    Project.getProjectActions()
+        .then()
+        .catch(next)
 })
+
+router.use(errorHandling);
 
 
 module.exports = router;
