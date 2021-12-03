3101a7fc15807cd4ec24e77a6644f412306e1756
danielaescutia07 <danielaescutia@yahoo.com>
2021-12-03 12:44:20 -0600

    project-router is wired and flusing out a console.log

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,6 +1,26 @@
 // Write your "projects" router here!
 const express = require('express');
 
-const router = express.Router()
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.json('it is working!')
+})
+router.get('/:id', (req, res) => {
+
+})
+router.post('/', (req, res) => {
+
+})
+router.put('/:id', (req, res) => {
+
+})
+router.delete('/:id', (req, res) => {
+
+})
+router.get('/:id/actions', (req, res) => {
+
+})
+
 
 module.exports = router;
